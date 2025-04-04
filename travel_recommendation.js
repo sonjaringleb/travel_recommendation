44af3ef4d85611e476a95bc334aadf85e0ed6226
Sonja Ringleb <Sonja.Ringleb@gmail.com>
2025-04-04 13:32:01 -0400

    JS Course final project: set destination type based on search input in js file

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,14 +3,40 @@ searchBtn = document.getElementById("searchBtn");
 console.log("Start JS script.");
 
 function getTravelRecommendation(event) {
-    event. preventDefault();
+     event.preventDefault();
 
     const apiUrl = "./travel_recommendation_api.json";
+    const keyword = document.getElementById("searchInput").value.toLowerCase();
+    
+    console.log("Keyword = " + keyword);
+
+    let destinationType = " ";
+
+    if(keyword === "beach" || keyword === "beaches"){
+        destinationType = "beach";
+    }
+    
+    if (keyword === "temple" || keyword === "temples"){
+        destinationType = "temple";
+    }
+
+    if (keyword === "country" || keyword === "countries"){
+        destinationType = "country";
+    }
+
+    if (keyword && destinationType === " "){
+        destinationType = "unknown"
+    }
+
+    console.log("Destination Type = " + destinationType);
 
     fetch(apiUrl)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
+        
+            
+           
 
         })
         .catch(error => {
