392c7389c7b272072f263e7bf007901350b4d6bb
Sonja.Ringleb <Sonja.Ringleb@gmail.com>
2025-04-05 18:24:28 -0400

    JS Course final project - Task 8 (keywords only)

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,28 +3,30 @@ searchBtn = document.getElementById("searchBtn");
 console.log("Start JS script.");
 
 function getTravelRecommendation(event) {
-     event.preventDefault();
+    event.preventDefault();
 
     const apiUrl = "./travel_recommendation_api.json";
     const keyword = document.getElementById("searchInput").value.toLowerCase();
-    
+
+    const searchResults = document.getElementById("searchResults");
+
     console.log("Keyword = " + keyword);
 
     let destinationType = " ";
 
-    if(keyword === "beach" || keyword === "beaches"){
-        destinationType = "beach";
-    }
-    
-    if (keyword === "temple" || keyword === "temples"){
-        destinationType = "temple";
+    if (keyword === "beach" || keyword === "beaches") {
+        destinationType = "beaches";
     }
 
-    if (keyword === "country" || keyword === "countries"){
-        destinationType = "country";
+    if (keyword === "temple" || keyword === "temples") {
+        destinationType = "temples";
     }
 
-    if (keyword && destinationType === " "){
+    if (keyword === "country" || keyword === "countries") {
+        destinationType = "countries";
+    }
+
+    if (keyword && destinationType === " ") {
         destinationType = "unknown"
     }
 
@@ -35,12 +37,61 @@ function getTravelRecommendation(event) {
         .then((data) => {
             console.log(data);
 
+            let recommendations;
+
+            if (destinationType === "countries") {
+                recommendations = [];
+                for (const country of data.countries) {
+                    for (const city of country.cities) {
+                        recommendations.push(city);
+                    }
+                }
+            } else {
+                recommendations = data[destinationType];
+            }
+
+
+            const searchResultsHTML = document.getElementById("searchResults");
+            // const recommendation = {
+            //     imageUrl: "/travel_recommendation/images/sydney.jpg",
+            //     name: "Sydney, Australia",
+            //     description: "A vibrant city known for its iconic landmarks like the Sydney Opera House and Sydney Harbour Bridge."
+            // };
+
+            const recommendationsHTML = recommendations.map((recommendation) => generateRecommendation(recommendation)).join();
+
+            searchResultsHTML.innerHTML = recommendationsHTML;
         })
         .catch(error => {
             console.error('Error:', error);
             resultDiv.innerHTML = 'An error occurred while fetching data.';
         });
 
+
 }
+
+
+
+
+
+function generateRecommendation(recommendation) {
+
+    //console.log(recommendation);
+
+    const htmlString = `<div class="recommendation">
+    <img class="destinationImage" src="${recommendation.imageUrl}"/>
+    <div class="destinationDescription">
+        <h3 class="destinationHeader">${recommendation.name}</h3>
+        <p class="destinationText">${recommendation.description}</p>
+        <button class ="visitBtn">Visit</button>
+    </div>
+</div>`
+
+    return htmlString;
+}
+
+// console.log(generateRecommendation(recommendation));
+
+
 
 searchBtn.addEventListener("click", getTravelRecommendation);
